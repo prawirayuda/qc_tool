d6d1907687c52305340e32709f826c3fd31207f0
Prawira yuda <muhammadprawirayuda@gmail.com>
2022-03-22 16:02:33 +0700

    Checkpoint1

--- aw2.py
+++ aw2.py
@@ -111,8 +111,11 @@ class Worker(QRunnable):
         
     @pyqtSlot()
     def run(self):
-        test = Context(QC_STATE_STANDBY(parent=None))
+        # test = CallMethodFromController()
+         
+        
         # self._parent.update("PASS")
+        test = Context(QC_STATE_STANDBY())
         test.pass_function()
         
         
@@ -152,11 +155,10 @@ class Controller:
 
 
 # the context class contains a _state that references the concrete state and setState method to change between states.
-class Context():
+class Context:
 
     _state = None
     def __init__(self, state: State) -> None:
-        super().__init__()
         self.setState(state)
 
     def setState(self, state: State):
@@ -173,7 +175,9 @@ class Context():
     def fail_function(self):
         self._state.fail_function()
         
-        
+    # def update(self):
+    #     self._state.update()
+
 
 class State(ABC):
     @property
@@ -187,24 +191,29 @@ class State(ABC):
     @abstractmethod
     def pass_function(self) -> None:
         pass
-
-    def fail_function(self):
+    
+    # @abstractmethod
+    def fail_function(self) -> None:
         pass
+    
+    # @abstractmethod
+    # def update(self):
+    #     pass
 
-
+class CallMethodFromController(Controller):
+    def __init__(self):
+        pass
+    
+        self.update("PASS")
+           
 class QC_STATE_STANDBY(State):
-    def __init__(self, parent, *args, **kwargs):
-        super(self.__class__,self).__init__(*args, **kwargs)
-        self._parent = parent
 
     def pass_function(self) -> None:
-        
         print("STATE : QC_STATE_STANDBY.")
         print("QC_STATE_STANDBY now changes the state of the context.")
-        # self._parent.update("PASS")
         time.sleep(2)
         # self.context.setState(QC_STATE_TEST_POWER_RAIL())
-        power_test = Context(QC_STATE_TEST_POWER_RAIL(parent=None))
+        power_test = Context(QC_STATE_TEST_POWER_RAIL())
         power_test.pass_function()
     
     def fail_function(self):
@@ -212,36 +221,39 @@ class QC_STATE_STANDBY(State):
         self.context.setState(QC_STATE_STANDBY())
 
     
-class QC_STATE_TEST_POWER_RAIL(State):
-    def __init__(self, parent, *args, **kwargs):
-        super(self.__class__,self).__init__(*args, **kwargs)
-        self._parent = parent
-        
+class QC_STATE_TEST_POWER_RAIL(State):   
     def pass_function(self) -> None:
         print("STATE : QC_STATE_TEST_POWER_RAIL.")
         ser = serial.Serial("COM20", 9600)
-        ser.write(b"{?P}")
+        ser.write(b"{P?}")
         data = ser.readline()
         print(data)
-        res = data.decode()
-        result = res.strip()
+        
+        input1 = data[1:5]
+        input2 = input1[0:2]
+        input3 = input1[2:]
+        
+        integer1 = int.from_bytes(input2,"little")
+        integer2 = int.from_bytes(input3,"little")
+        print(integer1)
+        print(integer2)
+        th = 400
         while True:
-            if result == "OK":
+            if integer1 > th and integer2 > th:
                 print("PASS")
-                # self._parent.parent.label1.setText("PASS")
-                tes = Controller(self)
-                tes.update("PASS")
                 break
             else:
-                
                 return self.fail_function()
 
         print("QC_STATE_TEST_POWER_RAIL wants to change the state of the context.")
-        time.sleep(5)
+        time.sleep(3)
         self.context.setState(QC_STATE_SENSOR())
+        sensor_test = Context(QC_STATE_SENSOR())
+        sensor_test.pass_function()
         
         
-    def fail_function(self):
+        
+    def fail_function(self) -> None:
         print("if fail back to standby mode")
         self.context.setState(QC_STATE_STANDBY())
         print("STANDBY MODE")
@@ -255,15 +267,12 @@ class QC_STATE_SENSOR(State):
         print("The context is in the state of QC_STATE_TEST_SENSOR.")
         print("QC_STATE_TEST_SENSOR wants to change the state of the context.")
         # time.sleep(5)
-        self.context.setState(QC_STATE_STANDBY())
+        # self.context.setState(QC_STATE_STANDBY())
 
 
         
         
 # app = Context(QC_STATE_STANDBY())
-# app.pass_function()    # this method is executed as in state 1
-# update = Notify()
-# update.update_test("PASS")
 # app.pass_function()    # this method is executed as in state 1
 # app.pass_function()    # this method is executed as in state 2
 
