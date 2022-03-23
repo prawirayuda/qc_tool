f47f63684a25363c91677014e0656d9054622ea6
Prawira yuda <muhammadprawirayuda@gmail.com>
2022-03-23 18:22:57 +0700

    transisi to test sensor has done next test tamper

--- aw2.py
+++ aw2.py
@@ -37,7 +37,7 @@ from PyQt5.QtCore import QObject, QThread, pyqtSignal, QRunnable, QThreadPool
 
 class TaskRow(IntEnum):
     TEST1 = 0
-    TEST2 = auto()
+    TEST_POWER_RAIL = auto()
     TEST3 = auto()
     TEST4 = auto()
 
@@ -50,7 +50,6 @@ class MainWindow(QMainWindow,QWidget):
         self.setWindowTitle("FSM")
         self.setFixedSize(500,200)
         self._controller = Controller(self)
-        # self._context = Context(self,parent=None)
         self.parent = parent
         
         self.container = QVBoxLayout()
@@ -81,8 +80,8 @@ class MainWindow(QMainWindow,QWidget):
         
         self.inner.addWidget(self.button1,TaskRow.TEST1,0,1,2)
         # self.inner.addWidget(self.label1,TaskRow.TEST1,1)
-        self.inner.addWidget(self.button2,TaskRow.TEST2,0)
-        self.inner.addWidget(self.label2,TaskRow.TEST2,1)
+        self.inner.addWidget(self.button2,TaskRow.TEST_POWER_RAIL,0)
+        self.inner.addWidget(self.label2,TaskRow.TEST_POWER_RAIL,1)
         self.inner.addWidget(self.button3,TaskRow.TEST3,0)
         self.inner.addWidget(self.label3,TaskRow.TEST3,1)
         self.inner.addWidget(self.button4,TaskRow.TEST4,0)
@@ -114,31 +113,23 @@ class Worker(QRunnable):
     def run(self):
         
         self._parent._parent.button1.setEnabled(False)
-        self._parent._parent.label3.setText("PASS")
-        # self._parent.update("PASS")
-        # if self._parent.update == "START TO PUSH BUTTON TEST POWER":
+        # self._parent._parent.label3.setText("PASS")
         standby = Context(QC_STATE_STANDBY(self))
         test_power_rail = Context(QC_STATE_TEST_POWER_RAIL(self))
-        sensor_test = Context(QC_STATE_SENSOR(self))
-        data_str = self._parent._parent.label3.text()
-        print(data_str)
+        test_sensor = Context(QC_STATE_SENSOR(self))
         standby.pass_function()
         test_power_rail.pass_function()
-        if data_str == "PASS":
-            sensor_test.pass_function()
-        # self._parent.update_label("PASS")
-            # sensor_test.pass_function()
-        # while True:
-        #     test_power_rail.pass_function()
-        #     if self._parent.update == "PASS":
-        #         sensor_test.pass_function()
-        #         break
-        # self._parent._parent.pass_button.setStyleSheet("background-color: {}".format("#fff"))
- 
-        
+        data_update = self._parent._parent.label_instruction.text()
+        print(data_update)
+        if data_update == "PASS":
+            self._parent._parent.label2.setText("PASS")
+            self._parent._parent.label3.setText("ON GOING")
+            test_sensor.pass_function()
+        elif data_update == "FAIL":
+            self._parent._parent.label2.setText("FAIL")
 
+            
         
-
 class Controller:
     def __init__(self, parent):
         self._parent = parent
@@ -170,8 +161,10 @@ class Controller:
         self.ui_standby()
 
     def ui_standby(self):
-        self._parent.label_instruction.setText("MODE STANDBY")
+        # self._parent.label_instruction.setText("MODE STANDBY")
         self._parent.button1.setEnabled(True)
+        self._parent.pass_button.setStyleSheet('background: palette(window)')
+        self._parent.fail_button.setStyleSheet('background: palette(window)')
 
 # the context class contains a _state that references the concrete state and setState method to change between states.
 class Context:
@@ -221,12 +214,11 @@ class QC_STATE_STANDBY(State):
     def pass_function(self) -> None:
         print("STATE : QC_STATE_STANDBY.")
         # print("QC_STATE_STANDBY now changes the state of the context.")
-        time.sleep(0.5)
         self._controller._parent.update("STANDBY MODE")
 
     def fail_function(self):
         print("if fail stay in standby mode")
-        self.context.setState(QC_STATE_STANDBY(self))
+        # self.context.setState(QC_STATE_STANDBY(self))
 
     
 class QC_STATE_TEST_POWER_RAIL(State):  
@@ -236,31 +228,31 @@ class QC_STATE_TEST_POWER_RAIL(State):
     
     def pass_function(self) -> None:
         print("STATE : QC_STATE_TEST_POWER_RAIL.")
-        # ser = serial.Serial("COM20", 9600)
-        # ser.write(b"{P?}")
-        # data = ser.readline()
+        ser = serial.Serial("COM20", 9600)
+        ser.write(b"{P?}")
+        data = ser.readline()
         # print(data)
-        # input1 = data[1:5]
-        # input2 = input1[0:2]
-        # input3 = input1[2:]
-        # integer1 = int.from_bytes(input2,"little")
-        # integer2 = int.from_bytes(input3,"little")
-        # th1 = 1.6
-        # th2 = 1.1
-        # while True:
-        #     val1 = (integer1/1023) * 1.8
-        #     val2 = (integer2/1023) * 1.2
-        #     print(val1)
-        #     print(val2)
-        #     if val1 > th1 and val2 > th2: #test pass value
-        #     # if val1 < th1 and val2 < th2: #test fail value
-        #         print("PASS")
-        #         self._controller._parent.update("PASS")
-        #         break
-        #     else:
-        #         print("FAIL")
-        #         self._controller._parent.update("FAIL")
-        #         return self.fail_function()
+        input1 = data[1:5]
+        input2 = input1[0:2]
+        input3 = input1[2:]
+        integer1 = int.from_bytes(input2,"little")
+        integer2 = int.from_bytes(input3,"little")
+        th1 = 1.6
+        th2 = 1.1
+        while True:
+            val1 = (integer1/1023) * 1.8
+            val2 = (integer2/1023) * 1.2
+            # print(val1)
+            # print(val2)
+            if val1 > th1 and val2 > th2: #test pass value
+            # if val1 < th1 and val2 < th2: #test fail value
+                print("PASS")
+                self._controller._parent.update("PASS")
+                break
+            else:
+                print("FAIL")
+                self._controller._parent.update("FAIL")
+                return self.fail_function()
                 
 
         print("QC_STATE_TEST_POWER_RAIL wants to change the state of the context.")
@@ -282,26 +274,44 @@ class QC_STATE_SENSOR(State):
         self._controller = controller
         
     def pass_function(self) -> None:
-        self._controller._parent._parent.pass_button.setStyleSheet("background-color: {}".format("#fff"))
+        # self._controller._parent._parent.pass_button.setStyleSheet("background-color: {}".format("#fff"))
         print("STATE : QC_STATE_TEST_SENSOR.")
-        # ser = serial.Serial("COM20", 9600)
-        # ser.write(b"{S?}")
-        # data = ser.readline()
-        # print(data)
-        # input1 = data[1:5]
-        # input2 = input1[0:2]
-        # input3 = input1[2:]
-        # integer1 = int.from_bytes(input2,"little")
-        # integer2 = int.from_bytes(input3,"little")
-        # print(integer1)
-        # print(integer2)  
+        ser = serial.Serial("COM20", 9600)
+        ser.write(b"{S?}")
+        data = ser.readline()
+        print(data)
+        input1 = data[1:5]
+        input2 = input1[0:2]
+        input3 = input1[2:]
+        integer1 = int.from_bytes(input2,"little")
+        integer2 = int.from_bytes(input3,"little")
+        print(integer1)
+        print(integer2)  
+        th1 = 1.6
+        th2 = 1.1
+        while True:
+            val1 = (integer1/1023) * 1.8
+            val2 = (integer2/1023) * 1.2
+            # print(val1)
+            # print(val2)
+            # if val1 > th1 and val2 > th2: #test pass value
+            if val1 < th1 and val2 < th2: #test fail value
+                print("PASS")
+                self._controller._parent.update("PASS")
+                break
+            else:
+                print("FAIL")
+                self._controller._parent.update("FAIL")
+                time.sleep(2)
+                return self.fail_function()      
         
-        print("QC_STATE_TEST_SENSOR wants to change the state of the context.")
+        print("QC_STATE_TEST_SENSOR wants to change to next state.")
 
     def fail_function(self) -> None:
         print("if fail back to standby mode")
         self.context.setState(QC_STATE_STANDBY(QC_STATE_STANDBY))
         print("STANDBY MODE")
+        self._controller._parent.handleNG()
         
         
 
